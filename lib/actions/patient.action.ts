e19e3235697b7d3c6a539d8dc0fcb8bff0464043
yayano <boussetta13yahia@gmail.com>
2024-08-30 02:29:08 +0200

    register patient successfully

--- lib/actions/patient.action.ts
+++ lib/actions/patient.action.ts
@@ -46,6 +46,7 @@ export const registerPatient = async ({
 }: RegisterUserParams) => {
   try {
     let file;
+
     if (identificationDocument) {
       const inputFile =
         identificationDocument &&
@@ -60,10 +61,10 @@ export const registerPatient = async ({
       PATIENT_COLLECTION_ID!,
       ID.unique(),
       {
-        identificationDocumentId: file?.$id || null,
-        identificationDocumentUrl: `${ENDPOINT!}/storage/buckets/${BUCKET_ID}/files/${
-          file?.$id
-        }/view?project=${PROJECT_ID}`,
+        identificationDocumentId: file?.$id ? file.$id : null,
+        identificationDocumentUrl: file?.$id
+          ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
+          : null,
         ...patient,
       }
     );
