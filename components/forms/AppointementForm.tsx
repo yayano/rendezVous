f411cd45847df42257326e639de4e3df110249eb
yayano <boussetta13yahia@gmail.com>
2024-08-30 19:13:21 +0100

    appointement

---
+++ components/forms/AppointementForm.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Form } from "@/components/ui/form";
+import CustomFormField from "../ui/CustomFormField";
+import SubmitButton from "../ui/SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.action";
+import { FormFieldType } from "./PatienForm";
+import { Doctors } from "@/constants";
+import { SelectItem } from "../ui/select";
+import Image from "next/image";
+
+const AppointementForm = ({
+  userId,
+  patientId,
+  type,
+}: {
+  userId: String;
+  patientId: String;
+  type: "create" | "cancel";
+}) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+      const userData = { name, email, phone };
+      const user = await createUser(userData);
+      console.log(user);
+      if (user) {
+        router.push(`/patients/${user.$id}/register`);
+      }
+    } catch (err) {
+      console.log("error from create", err);
+    }
+    setIsLoading(false);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">New appointement </h1>
+          <p className="text-dark-700">
+            Réservez votre premier rendez-vous dans 10 seconds.
+          </p>
+        </section>
+        {type !== "cancel" && (
+          <>
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.SELECT}
+              name="primaryPhysician"
+              label="Médecin"
+              placeholder="Selectioné un médecin"
+            >
+              {Doctors.map((doctor) => (
+                <SelectItem key={doctor.name} value={doctor.name}>
+                  <div className="flex cursor-pointer items-center gap-2">
+                    <Image
+                      src={doctor.image}
+                      width={32}
+                      height={32}
+                      alt={doctor.name}
+                      className="rounded-full border border-dark-500"
+                    />
+                    <p>{doctor.name}</p>
+                  </div>
+                </SelectItem>
+              ))}
+            </CustomFormField>
+            <CustomFormField
+              fieldType={FormFieldType.DATE_PICKER}
+              control={form.control}
+              name="schedule"
+              label="Expected appointment date"
+              showTimeSelect
+              dateFormat="MM/dd/yyyy  -  h:mm aa"
+            />
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.TEXTAREA}
+              name="reason"
+              label="reason for appointment"
+              placeholder="Enter reason for appointment"
+            />
+            <CustomFormField
+              control={form.control}
+              fieldType={FormFieldType.TEXTAREA}
+              name="notes"
+              label="Notes"
+              placeholder="Enter notes"
+            />
+          </>
+        )}
+        {type === "cancel" && (
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.TEXTAREA}
+            name="cancellationReason"
+            label="Reason for cancellation"
+            placeholder="Enter reason for cancellation"
+          />
+        )}
+        <SubmitButton
+          isLoading={isLoading}
+          className={`${type === "cancel"} ?
+        'shad-danger-btn' : 'shad-primary-btn'} w-full
+        `}
+        >
+          Commencer
+        </SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default AppointementForm;
