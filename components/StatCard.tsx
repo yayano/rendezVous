69c196f4a7481d49b7b75bf0cdd6ca86a46bc6ed
yayano <boussetta13yahia@gmail.com>
2024-09-01 22:13:59 +0100

    create admin panel

---
+++ components/StatCard.tsx
@@ -0,0 +1,11 @@
+interface StatCardProps {
+  type: "appointments" | "pending" | "cancelled";
+  count: number;
+  label: string;
+  icon: string;
+}
+const StatCard = ({ count = 0, label, icon, type }: StatCardProps) => {
+  return <div>StatCard</div>;
+};
+
+export default StatCard;
